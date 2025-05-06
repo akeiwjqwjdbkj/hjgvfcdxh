4bb96c81dfa81feffb9f57e9c5f9217630908df5
akeiwjqwjdbkj <66285938+akeiwjqwjdbkj@users.noreply.github.com>
2025-05-06 19:25:11 -0400

    Added Book model

--- catalog/models.py
+++ catalog/models.py
@@ -9,6 +9,8 @@ from django.db.models.functions import Lower # Return lower case value
 class Genre(models.Model):
 	"""Model representing a book genre."""
 
+	# Fields
+
 	name = models.CharField(
 		max_length=200,
 		unique=True,
@@ -29,3 +31,31 @@ class Genre(models.Model):
 			name='genre_name_case_insensitive_unique',
 			violation_error_message="Genre already exists (case insensitive match)"
 		)]
+
+class Book(models.Model):
+	"""Model representing a book (but not a specific copy of a book)."""
+
+	# Fields
+
+	title = models.CharField(max_length=200)
+	# Book can only have one author, but authors can have multiple books
+	# Author to be declared as a string as it hasn't been declared yet
+	author = models.ForeignKey('Author', on_delete=models.RESTRICT, null=True)
+	summary = models.TextField(max_length=1000, help_text='Enter a brief descrption of the book')
+	isbn = models.CharField(
+		'ISBN',
+		max_length=13,
+		unique=True,
+		help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">''">ISBN number</a>'
+	)
+	# Genre can contain many books, and books can cover many genres
+	# Genre class has already been defined so we can specify object above
+	genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return self.title
+	
+	def get_absolute_url(self):
+		'''Returns the URL to access a detail record for this book.'''
+		return reverse('book-detail', args=[str(self.id)])
