ce526d9946fabea9572899cdd47273c8e8a6d03d
akeiwjqwjdbkj <66285938+akeiwjqwjdbkj@users.noreply.github.com>
2025-05-10 19:54:20 -0400

    Added model registrations

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Author, Genre, Book, BookInstance, Language
+
 # Register your models here.
+
+# username: asdfasdf, email@email.com
+# password: p5bbw0rd
+
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Language)
