5d40d67cfc0d25daffe532cc9c3d835859e3b074
akeiwjqwjdbkj <66285938+akeiwjqwjdbkj@users.noreply.github.com>
2025-05-11 16:50:40 -0400

    Added admin panels

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,8 +7,25 @@ from .models import Author, Genre, Book, BookInstance, Language
 # username: asdfasdf, email@email.com
 # password: p5bbw0rd
 
-admin.site.register(Author)
-admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
-admin.site.register(Language)
+class AuthorAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+	pass
+
+class BookAdmin(admin.ModelAdmin):
+	pass
+
+class BookInstanceAdmin(admin.ModelAdmin):
+	pass
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Author, AuthorAdmin)
+# admin.site.register(Genre)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+# admin.site.register(Language)
