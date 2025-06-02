bcfe4b62c0a3ca19ba1ea2b144899bc82a597e4e
akeiwjqwjdbkj <66285938+akeiwjqwjdbkj@users.noreply.github.com>
2025-06-02 11:08:01 -0400

    Added various properties to admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -7,18 +7,39 @@ from .models import Author, Genre, Book, BookInstance, Language
 # username: asdfasdf, email@email.com
 # password: p5bbw0rd
 
+class BooksInstanceInline(admin.TabularInline):
+	model = BookInstance
+	extra = 0
+
+class BookInline(admin.StackedInline):
+	model = Book
+	extra = 0
+
+
 class AuthorAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+	inlines = [BookInline]
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
 	pass
 
 class BookAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('title', 'author', 'display_genre')
+
+	inlines = [BooksInstanceInline]
 
 class BookInstanceAdmin(admin.ModelAdmin):
-	pass
+	list_display = ('book', 'status', 'due_back', 'id')
+	list_filter = ('status', 'due_back')
+
+	fieldsets = (
+		(None, { 'fields' : ('book', 'imprint', 'id') }),
+		('Availability', { 'fields' : ('status', 'due_back') }),
+	)
 
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
