347f0d4307bdfec0d2ee511fbe7e2dc9ce868f05
akeiwjqwjdbkj <66285938+akeiwjqwjdbkj@users.noreply.github.com>
2025-07-16 03:09:24 -0400

    Added tests for Author model

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from catalog.models import Author
+
 class TestClassI(TestCase):
 	@classmethod
 	def setUpTestData(cls):
@@ -14,10 +16,65 @@ class TestClassI(TestCase):
 		print('Method: test_false_is_false.')
 		self.assertFalse(False)
 	
-	def test_false_is_true(self):
-		print('Method: test_false_is_true.')
-		self.assertTrue(False)
+	#def test_false_is_true(self):
+		#print('Method: test_false_is_true.')
+		#self.assertTrue(False)
 
 	def test_one_plus_one_equals_two(self):
 		print('Method: one_plus_one_equals_two.')
 		self.assertEqual(1 + 1, 2)
+
+class AuthorModelTest(TestCase):
+	@classmethod
+	def setUpTestData(cls):
+		# Set up non-modified objects used by all test methods
+		Author.objects.create(first_name='Big', last_name='Bob')
+	
+	def test_first_name_label(self):
+		# produce author object
+		author = Author.objects.get(id=1)
+		# obtain metadata for required field and use to query required data
+		# obtain verbose name from meta field; typical field (author.first_name) is a string
+		field_label = author._meta.get_field('first_name').verbose_name
+		# compare value to expected result
+		# failure of test indicates label using assertEqual
+		self.assertEqual(field_label, 'first name')
+
+	#
+	def test_last_name_label(self):
+		author = Author.objects.get(id=1)
+		field_label = author._meta.get_field('last_name').verbose_name
+		self.assertEqual(field_label, 'last name')
+
+	#
+	def test_date_of_birth_label(self):
+		author = Author.objects.get(id=1)
+		field_label = author._meta.get_field('date_of_birth').verbose_name
+		self.assertEqual(field_label, 'date of birth')
+	
+	def test_date_of_death_label(self):
+		author = Author.objects.get(id=1)
+		field_label = author._meta.get_field('date_of_death').verbose_name
+		self.assertEqual(field_label, 'Died')
+	
+	def test_first_name_max_length(self):
+		author = Author.objects.get(id=1)
+		max_length = author._meta.get_field('first_name').max_length
+		self.assertEqual(max_length, 100)
+	
+	#
+	def test_last_name_max_length(self):
+		author = Author.objects.get(id=1)
+		max_length = author._meta.get_field('last_name').max_length
+		self.assertEqual(max_length, 100)
+	
+	def test_object_name_is_last_name_comma_first_name(self):
+		author = Author.objects.get(id=1)
+		expected_objecct_name = f'{author.last_name}, {author.first_name}'
+		self.assertEqual(str(author), expected_objecct_name)
+	
+	def test_get_absolute_url(self):
+		author = Author.objects.get(id=1)
+		# note : will also fail with undefined URLConf
+		# for id=1
+		self.assertEqual(author.get_absolute_url(), '/catalog/authors/1')
